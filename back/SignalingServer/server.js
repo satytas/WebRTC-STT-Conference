5d3b723fab935d0fe64892d9237aabacb3f8c49b
satytas <yokselk@gmail.com>
2025-03-26 09:09:30 +0200

    Add .gitattributes and isolated SignalingServer in the back

--- back/SignalingServer/server.js
+++ back/SignalingServer/server.js
@@ -1,137 +1,117 @@
-const WebSocket = require('ws');
-const EventTypes = Object.freeze({
-    // Client to Server events
-    CLIENT_JOIN_ROOM: "client:join-room",
-    CLIENT_CREATE_ROOM: "client:create-room",
-    CLIENT_VALIDATE_ROOM: "client:validate-room",
-    CLIENT_VALIDATE_PASSWORD: "client:validate-password",
-    CLIENT_DISCONNECT: "client:disconnect",
+import { WebSocketServer } from 'ws';
+import { EventTypes } from '../../front/src/utils/EventTypes.js';
 
-    // Server to Client events
-    SERVER_WELCOME: "server:welcome",
-    SERVER_ROOM_CREATED: "server:room-created",
-    SERVER_ROOM_VALIDATION: "server:room-validation",
-    SERVER_PASSWORD_VALIDATION: "server:password-validation",
-    SERVER_NEW_USER: "server:new-user",
-
-    PEER_OFFER: "peer:offer",
-    PEER_ANSWER: "peer:answer",
-    PEER_ICE_CANDIDATE: "peer:ice-candidate",
-    PEER_USER_LEFT: "peer:user-left"
-});
-
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocketServer({ port: 8080 });
 
 const rooms = new Map(); // Map<roomId, Map<ws, userId>>
 
 wss.on('connection', ws => {
-    ws.on('message', message => {
-        const data = JSON.parse(message);
-        
-        switch (data.type) {
-            //client to server
-            case EventTypes.CLIENT_JOIN_ROOM: {
-                const { roomId, userId } = data;
-                const room = rooms.get(roomId) || rooms.set(roomId, new Map()).get(roomId);
+  ws.on('message', message => {
+    const data = JSON.parse(message);
 
-                room.set(ws, userId);
-                ws.send(JSON.stringify({ type: EventTypes.SERVER_WELCOME, userId, roomId }));
-                console.log(`User ${userId} joined room ${roomId}`);
-                console.log(`Room ${roomId} has ${room.size} users`);
-                console.log('this are the users inside this room: ');
-                for (const [client, id] of room)
-                    console.log(`User ${id} inside room ${roomId}`);
+    switch (data.type) {
+      //client to server
+      case EventTypes.CLIENT_JOIN_ROOM: {
+        const { roomId, userId } = data;
+        const room = rooms.get(roomId) || rooms.set(roomId, new Map()).get(roomId);
 
-            
-                for (const [client] of room)
-                    if (client !== ws && client.readyState === WebSocket.OPEN) 
-                        client.send(JSON.stringify({ type: EventTypes.SERVER_NEW_USER, userId }));
-            
-            } break;
-        
-            case EventTypes.CLIENT_CREATE_ROOM: {
-                const roomId = Math.random().toString(36).slice(2, 10);
-                const room = new Map();
-                console.log("PASSWORD: " ,data.password);
-                room.password = (data.password || null);
-                rooms.set(roomId, room);
+        room.set(ws, userId);
+        ws.send(JSON.stringify({ type: EventTypes.SERVER_WELCOME, userId, roomId }));
+        console.log(`User ${userId} joined room ${roomId} (${room.size}) | `);
 
-                ws.send(JSON.stringify({
-                    type: EventTypes.SERVER_ROOM_CREATED,
-                    roomId: roomId
-                }));
-            } break;
+        for (const [client] of room)
+          if (client !== ws && client.readyState === WebSocket.OPEN)
+            client.send(JSON.stringify({ type: EventTypes.SERVER_NEW_USER, userId }));
 
-            case EventTypes.CLIENT_VALIDATE_ROOM: {
-                const room = rooms.get(data.roomId);
-            
-                ws.send(JSON.stringify({
-                    type: EventTypes.SERVER_ROOM_VALIDATION,
-                    exists: !!room,
-                    passwordRequired: room ? room.password !== null : false,
-                    passwordCorrect: room ? data.password === room.password : false
-                }));
-            } break;
-            
-            case EventTypes.CLIENT_VALIDATE_PASSWORD: {
-                const room = rooms.get(data.roomId);
-                if (!room) {
-                    ws.send(JSON.stringify({
-                        type: EventTypes.SERVER_PASSWORD_VALIDATION,
-                        success: false,
-                        error: "Room not found"
-                    }));
-                    return;
-                }
-                ws.send(JSON.stringify({
-                    type: EventTypes.SERVER_PASSWORD_VALIDATION,
-                    success: room.password === data.password
-                }));
-            } break;
+      } break;
 
-            //client to client
-            default: {
-                for (const [roomId, users] of rooms) {
-                    if (users.has(ws)) {
-                        const senderId = users.get(ws);
-                        for (const [client, id] of users) {
-                            if (id === data.target && client.readyState === WebSocket.OPEN)
-                                client.send(JSON.stringify({ ...data, from: senderId }));
-                        } break;
-                    }
-                }
-            } break;
+      case EventTypes.CLIENT_CREATE_ROOM: {
+        const roomId = Math.random().toString(36).slice(2, 10);
+        const room = new Map();
+        console.log("PASSWORD: ", data.password);
+        room.password = (data.password || null);
+        rooms.set(roomId, room);
+
+        ws.send(JSON.stringify({
+          type: EventTypes.SERVER_ROOM_CREATED,
+          roomId: roomId
+        }));
+      } break;
+
+      case EventTypes.CLIENT_VALIDATE_ROOM: {
+        const room = rooms.get(data.roomId);
+
+        ws.send(JSON.stringify({
+          type: EventTypes.SERVER_ROOM_VALIDATION,
+          exists: !!room,
+          passwordRequired: room ? room.password !== null : false,
+          passwordCorrect: room ? data.password === room.password : false
+        }));
+      } break;
+
+      case EventTypes.CLIENT_VALIDATE_PASSWORD: {
+        const room = rooms.get(data.roomId);
+
+        if (!room) {
+          ws.send(JSON.stringify({
+            type: EventTypes.SERVER_PASSWORD_VALIDATION,
+            success: false,
+            error: "Room not found"
+          }));
+          return;
         }
-    });
 
-    ws.on('close', () => {
+        ws.send(JSON.stringify({
+          type: EventTypes.SERVER_PASSWORD_VALIDATION,
+          success: room.password === data.password
+        }));
+      } break;
+
+      //client to client
+      default: {
         for (const [roomId, users] of rooms) {
-            if (users.has(ws)) {
-                const userId = users.get(ws);
-                users.delete(ws);
-                
-                console.log(`User ${userId} disconnected from room ${roomId} (WebSocket closed)`);
-                
-                if (users.size > 0) {
-                    for (const [client, id] of users) {
-                        if (client.readyState === WebSocket.OPEN) {
-                            client.send(JSON.stringify({ 
-                                type: EventTypes.PEER_USER_LEFT, 
-                                userId: userId 
-                            }));
-                        }
-                    }
-                }
-                
-                if (users.size === 0) {
-                    rooms.delete(roomId);
-                    console.log(`Room ${roomId} deleted because it's empty`);
-                }
-                
-                break;
+          if (users.has(ws)) {
+            const senderId = users.get(ws);
+            
+            for (const [client, id] of users) {
+              if (id === data.target && client.readyState === WebSocket.OPEN)
+                client.send(JSON.stringify({ ...data, from: senderId }));
             }
+            break;
+          }
         }
-    });
+      } break;
+    }
+  });
+
+  ws.on('close', () => {
+    for (const [roomId, users] of rooms) {
+      if (users.has(ws)) {
+        const userId = users.get(ws);
+        users.delete(ws);
+
+        console.log(`User ${userId} disconnected from room ${roomId} (WebSocket closed)`);
+
+        if (users.size > 0) {
+          for (const [client, id] of users) {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify({
+                type: EventTypes.PEER_USER_LEFT,
+                userId: userId
+              }));
+            }
+          }
+        }
+
+        if (users.size === 0) {
+          rooms.delete(roomId);
+          console.log(`Room ${roomId} deleted because it's empty`);
+        }
+
+        break;
+      }
+    }
+  });
 });
 
 console.log('WebSocket server running on ws://localhost:8080');
